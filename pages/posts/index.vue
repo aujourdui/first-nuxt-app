18781ea3580e146a73561cac43d3adfb65835d1d
kyosuke <kyosuke.ito@gmail.com>
2022-03-29 16:45:18 -0700

    basic error handling

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -5,12 +5,35 @@ export default {
   components: {
     PostList,
   },
+  asyncData(context, callback) {
+    return new Promise().catch((e) => context.error(new Error()));
+    setTimeout(() => {
+      callback(null, {
+        loadedPosts: [
+          {
+            id: "1",
+            title: "First post",
+            previewText: "This is first post",
+            thumbnail:
+              "https://youmatter.world/app/uploads/sites/2/2019/11/tech-planet.jpg",
+          },
+          {
+            id: "2",
+            title: "Second post",
+            previewText: "This is Second post",
+            thumbnail:
+              "https://youmatter.world/app/uploads/sites/2/2019/11/tech-planet.jpg",
+          },
+        ],
+      });
+    }, 1500);
+  },
 };
 </script>
 
 <template>
   <div class="posts-page">
-    <PostList />
+    <PostList :posts="loadedPosts" />
   </div>
 </template>
 
