6efbd7d8e54222c24652a82dd549083a32bfd7ba
kyosuke <kyosuke.ito@gmail.com>
2022-03-29 13:38:50 -0700

    create dynamic route

--- components/Posts/PostList.vue
+++ components/Posts/PostList.vue
@@ -10,32 +10,26 @@ export default {
       type: Boolean,
       default: false,
     },
+    posts: {
+      type: Array,
+      required: true,
+    },
   },
 };
 </script>
 
 <template>
+  <!-- https://youmatter.world/app/uploads/sites/2/2019/11/tech-planet.jpg -->
   <section class="post-list">
     <PostPreview
-      id="1"
+      v-for="post in posts"
+      :key="post.id"
+      :id="post.id"
       :is-admin="isAdmin"
-      thumbnail="https://youmatter.world/app/uploads/sites/2/2019/11/tech-planet.jpg"
-      title="Hello world"
-      previewText="This is my first post!"
+      :thumbnail="post.thumbnail"
+      :title="post.title"
+      :previewText="post.previewText"
     />
-    <PostPreview
-      id="2"
-      :is-admin="isAdmin"
-      thumbnail="https://youmatter.world/app/uploads/sites/2/2019/11/tech-planet.jpg"
-      title="Hello world second"
-      previewText="This is my second post!"
-    />
-    <PostPreview
-      id="3"
-      :is-admin="isAdmin"
-      thumbnail="https://youmatter.world/app/uploads/sites/2/2019/11/tech-planet.jpg"
-      title="Hello world third"
-      previewText="This is my third post!"
     />
   </section>
 </template>
