a0ddd10ce869fa0cf2f24169b8601f8b8589050c
kyosuke <kyosuke.ito@gmail.com>
2022-03-29 18:23:40 -0700

    get data from firebase server

--- store/index.js
+++ store/index.js
@@ -1,4 +1,5 @@
 import Vuex from "vuex";
+import axios from "axios";
 
 const createStore = () => {
   return new Vuex.Store({
@@ -12,27 +13,17 @@ const createStore = () => {
     },
     actions: {
       nuxtServerInit(vuexContext, context) {
-        return new Promise((resolve, reject) => {
-          setTimeout(() => {
-            vuexContext.commit("setPosts", [
-              {
-                id: "1",
-                title: "First post",
-                previewText: "This is first post",
-                thumbnail:
-                  "https://youmatter.world/app/uploads/sites/2/2019/11/tech-planet.jpg",
-              },
-              {
-                id: "2",
-                title: "Second post",
-                previewText: "This is Second post",
-                thumbnail:
-                  "https://youmatter.world/app/uploads/sites/2/2019/11/tech-planet.jpg",
-              },
-            ]);
-            resolve();
-          }, 1500);
-        });
+        return axios
+          .get("https://nuxt-blog-121f5-default-rtdb.firebaseio.com/posts.json")
+          .then((res) => {
+            const postsArray = [];
+            for (const key in res.data) {
+              postsArray.push({ ...res.data[key], id: key });
+            }
+
+            vuexContext.commit("setPosts", postsArray);
+          })
+          .catch((e) => context.error(e));
       },
       setPosts(vuexContext, posts) {
         vuexContext.commit("setPosts", posts);
